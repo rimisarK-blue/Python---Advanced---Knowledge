07820642a332f0155978af9da7056b16543fb1d3
rimisark_blue <rimisark92@gmail.com>
2021-02-21 18:47:01 +0000

    update

---
+++ Lists as Stacks and Queues/Lab/Solutions/03. Supermarket.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+names = deque()
+
+while True:
+    command = input()
+    if command == 'End':
+        print(f"{len(names)} people remaining.")
+        break
+    elif command == 'Paid':
+        while names:
+            print(names.popleft())
+    else:
+        names.append(command)
